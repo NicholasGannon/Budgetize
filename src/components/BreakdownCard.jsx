1ae0b4abc107f8b5ebee57484fea66475b2eb8ae
Nick Gannon <NGannon@kamsoncorp.com>
2024-08-26 12:49:15 -0400

    Incorporated state to subtract Income from spent

--- src/components/BreakdownCard.jsx
+++ src/components/BreakdownCard.jsx
@@ -1,9 +1,18 @@
-export default function BreakdownCard() {
+export default function BreakdownCard({setSpending}) {
+    const handleChange = (e) => {
+        setSpending(Number(e.target.value));
+    }
+
     return (
         <div className="flex flex-col gap-4 outline outline-1 outline-gray-200 p-4 rounded font-Raleway" style={{gridArea: "2 / 1 / 4 / 3"}}>
             <h2 className="text-2xl font-medium">Spending Breakdown</h2>
             <p className="text-base font-light">Add new categories for your monthly spending, and Budgetize will break down the amount relative to your monthly income.</p>
-            <input className="text-base indent-1 outline outline-1 outline-gray-300 rounded-sm" type="number" placeholder="Enter amount here"/>
+            <input
+                className="text-base indent-1 outline outline-1 outline-gray-300 rounded-sm" 
+                type="number" 
+                placeholder="Enter amount here"
+                onChange={handleChange}
+            />
         </div>
     )
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import IncomeCard from "./IncomeCard"
 import BreakdownCard from "./BreakdownCard"
 import RemainingCard from "./RemainingCard"
 import Visuals from "./Visuals"
 
 export default function Dashboard() {
+    const [income, setIncome] = useState(0);
+    const [spending, setSpending] = useState(0);
+
+    // Calculate remaining amount
+    const remainingAmount = income - spending;
+
     return (
         <section className="flex flex-col lg:grid grid-cols-5 grid-rows-4 gap-4 pb-12">
-            <IncomeCard />
-            <BreakdownCard />
-            <RemainingCard />
+            <IncomeCard setIncome={setIncome} />
+            <BreakdownCard setSpending={setSpending} />
+            <RemainingCard remainingAmount={remainingAmount} />
             <Visuals />
         </section>
     )
